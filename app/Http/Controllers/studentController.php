d8413da53f052ce23f476edc4a52348c2b649f3f
MitchWijt <mitchel@wijt.net>
2020-11-26 23:08:14 -0400

    feat: Added a POST, PUT and DELETE endpoint for students

--- app/Http/Controllers/studentController.php
+++ app/Http/Controllers/studentController.php
@@ -8,11 +8,13 @@ use App\Services\Student\StudentService as StudentService;
 
 class studentController extends Controller
 {
-    public function studentRequestHandler(Request $request, StudentService $studentService, $studentId) {
+    public function studentRequestHandler(Request $request, StudentService $studentService) {
         $requestMethod = $request->method();
-
         switch($requestMethod) {
-            case "GET" : return $studentService->getStudentJsonData($studentId);
+            case "GET" : return $studentService->getStudentJsonData($request);
+            case "PUT" : return $studentService->updateStudentJsonData($request);
+            case "POST" : return $studentService->addNewStudent($request);
+            case "DELETE" : return $studentService->deleteStudent($request);
         }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/student/{id}', 'StudentController@studentRequestHandler');
+
+
